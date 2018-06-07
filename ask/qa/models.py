83832cb673b0e8c91eb1d00cc114a430590d66ce
Timerbayev <account_service@bk.ru>
2018-06-07 18:08:26 +0300

    07062018 old

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,24 +3,24 @@ from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.order_by('-added_add')
+        return self.order_by('-added_at')
 
     def popular(self):
         return self.order_by('-rating')
 
 # Create your models here.
 class Question (models.Model):
-    object = QuestionManager()
+    objects = QuestionManager()
     title = models.CharField(max_length=50, blank=True, null=True, default=None)
     text = models.TextField()
-    added_add = models.DateTimeField(blank=True, null=True)
+    added_at = models.DateTimeField(blank=True, null=True)
     rating = models.IntegerField(default=0, blank=True)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='likes')
 
 class Answer (models.Model):
     text = models.TextField()
-    added_add = models.DateTimeField(blank=True, null=True)
+    added_at = models.DateTimeField(blank=True, null=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
